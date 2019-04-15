a6958453447f8b18cdf4e293403bfee7c6929507
Roman Kanafotskyi <roman95roman@gmail.com>
2019-04-15 12:59:48 +0300

    add component to create book

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   // TODO: style header
   return(
     <div>
+      <Link to={"/add-book"}>Add Book</Link>
       HEADER
     </div>
   )
