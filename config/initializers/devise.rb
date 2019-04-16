fc502f29377968ddac010e9a72965a8a2305e993
Roman Kanafotskyi <roman95roman@gmail.com>
2019-04-16 12:49:53 +0300

    remove facebook app keys

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -20,8 +20,5 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-
-  config.omniauth :facebook, '853438291662050', 'd26b7b00c8f8ba1a7ab71e01690fffb3',
-  provider_ignores_state: true, token_params: { parse: :json }
   config.navigational_formats = []
 end
