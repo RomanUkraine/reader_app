501917671728d551c4a56f4d26cac2909930f2a4
Roman Kanafotskyi <roman95roman@gmail.com>
2019-05-01 21:43:51 +0300

    added rspec and generated test files

---
+++ spec/controllers/assigned_books_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe AssignedBooksController, type: :controller do
+
+end
---
+++ spec/controllers/read_entries_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe ReadEntriesController, type: :controller do
+
+end
