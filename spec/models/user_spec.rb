7042708f044f5672730ef8bc759a77d0238fe531
Roman Kanafotskyi <roman95roman@gmail.com>
2019-05-02 01:28:12 +0300

    added test for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "associations" do
+    [:assigned_books, :read_entries].each do |assoc|
+      it { should have_many(assoc) }
+    end
+    it { should have_many(:books).through(:assigned_books) }
+  end
 end
